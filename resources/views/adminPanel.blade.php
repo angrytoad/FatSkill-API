27649e8d7bd77e246ddaa931cc60d7381d9d55de
Thoams Freeborough <thomas@hotsnapper.com>
2016-12-12 20:26:02 +0000

    [Update Admin Panel] Just some styling

--- resources/views/adminPanel.blade.php
+++ resources/views/adminPanel.blade.php
@@ -5,17 +5,19 @@
     <h1>Admin Panel</h1>
     <p>Keeping track of the numbers</p>
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div>
             <div class="panel panel-default">
                 <div class="panel-heading">Actions</div>
                 <div class="panel-body">
-                    <a href="/register">Register New Admin Account</a>
+                    <a href="/register">
+                        <button class="btn btn-info">Register A New Admin Account</button>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div>
             <div class="panel panel-default">
                 <div class="panel-heading">Statistics</div>
                 <div class="panel-body">
